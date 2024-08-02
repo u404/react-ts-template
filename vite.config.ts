f876b1feabad323c82524a207ce56fd84da4781c
张光辉 <ltxwy@hotmail.com>
2024-08-02 10:36:20 +0800

    feat: 多环境env配置; eslint + prettier 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, import.meta.url, "");
+  return {
+    base: env.BASE_URL || "",
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://10.0.0.69:8081/",
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/urule/, '')
+        },
+      },
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+});
