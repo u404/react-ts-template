9e539b3ddcfd9650fa7edfd128956b4afb263e81
张光辉 <ltxwy@hotmail.com>
2024-08-02 13:47:33 +0800

    feat: 移除冗余代码；sass支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,34 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, import.meta.url, "");
+  const env = loadEnv(mode, import.meta.url, '')
   return {
-    base: env.BASE_URL || "",
+    base: env.BASE_URL || '',
     server: {
       proxy: {
-        "/api": {
-          target: "http://10.0.0.69:8081/",
-          changeOrigin: true,
+        '/api': {
+          target: 'http://10.0.0.69:8081/',
+          changeOrigin: true
           // rewrite: (path) => path.replace(/^\/urule/, '')
-        },
-      },
+        }
+      }
     },
     plugins: [react()],
     resolve: {
       alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     },
-  };
-});
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/variables.scss";'
+        }
+      }
+    }
+  }
+})
