7bac0232e61b8a292d2a8aa8f71126a99be14715
张光辉 <ltxwy@hotmail.com>
2024-08-06 15:21:05 +0800

    feat: 增加 inject plugin 的预配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import mockDevServerPlugin from 'vite-plugin-mock-dev-server'
 import pxToRemOrVwPlugin from 'vite-plugin-px-rem-vw'
+import inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -28,6 +29,10 @@ export default defineConfig(({ mode }) => {
           mediaQuery: false // 媒体查询里的不转换
         }
       }),
+      // // 自动引入全局库，相当于在每个文件顶部自动加上了 import jQuery from 'jquery'
+      // inject({
+      //   jQuery: 'jquery' // 注意需要同时配置eslint
+      // }),
       mockDevServerPlugin()
     ],
     resolve: {
