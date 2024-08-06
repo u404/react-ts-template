d6f3b1cf0167937a0687d03cd95f15ed0df9e74d
张光辉 <ltxwy@hotmail.com>
2024-08-06 15:18:48 +0800

    feat: 移动端px到vw自动转换支持

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import mockDevServerPlugin from 'vite-plugin-mock-dev-server'
+import pxToRemOrVwPlugin from 'vite-plugin-px-rem-vw'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,7 +19,17 @@ export default defineConfig(({ mode }) => {
         }
       }
     },
-    plugins: [react(), mockDevServerPlugin()],
+    plugins: [
+      react(),
+      pxToRemOrVwPlugin({
+        type: 'vw',
+        options: {
+          viewportWidth: 375,
+          mediaQuery: false // 媒体查询里的不转换
+        }
+      }),
+      mockDevServerPlugin()
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
